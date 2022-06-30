30e01640d27b9117f069877a0b2d2728a4208b74
pos1234 <paulosegebeyehu5@gmail.com>
2022-06-30 13:39:58 -0700

    modified index js and html

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react'
+import ReactDOM from 'react-dom'
+class Myapp extends Component{
+    render(){
+    return (
+        <div className="der">
+           hi
+        </div>
+    )
+    }
+}
+
+ReactDOM.render(<Myapp />,document.getElementById('root'))
+
+
